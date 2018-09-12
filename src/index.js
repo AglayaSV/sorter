389538d3e0a66cc804c1ad942d7a6fe46f319cc1
aglaya <aglayastunkus1@gmail.com>
2018-09-12 10:48:17 +0300

    change an implementation of the sort method

--- src/index.js
+++ src/index.js
@@ -1,58 +1,42 @@
 class Sorter {
     constructor() {
         this.collection = [];
-
-
-        // your implementation
     }
 
     add(element) {
-        // your implementation
         this.collection.push(element);
     }
 
     at(index) {
         return this.collection[index];
-
-
-        // your implementation
     }
 
     get length() {
-        // your implementation
         return this.collection.length;
     }
 
     toArray() {
-        // your implementation
         return this.collection;
     }
 
-     standardComparator(a, b) {
+    standardComparator(a, b) {
         return a - b;
     };
 
-
     sort(indices) {
+        indices.sort((a, b) => a - b);
 
-     let indexesArr = indices.sort(function(a, b) {
-            return a - b});
-        let  tempArr =[];
-        for (let i = 0; i < indexesArr.length; i++){
-            tempArr.push(this.collection[indexesArr[i]]);
-        }
-        tempArr.sort(this.standardComparator);
-        for (let i = 0; i < indexesArr.length; i++){
-            this.collection.splice(indexesArr[i],1,tempArr[i]);
-        }
+        let tempArray = indices.map(item => this.collection[item])
+                               .sort(this.standardComparator);
 
+        let indicesLength = indices.length;
+        for (let i = 0; i < indicesLength; i++) {
+            this.collection.splice(indices[i], 1, tempArray[i]);
+        }
     }
 
     setComparator(compareFunction) {
-        // your implementation
         this.standardComparator = compareFunction;
-
-
     }
 }
 
